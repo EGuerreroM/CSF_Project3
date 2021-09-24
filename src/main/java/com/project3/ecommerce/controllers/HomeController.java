47910da7ce687cb857890735663f0cad5d05e882
EGuerreroM <72275181+EGuerreroM@users.noreply.github.com>
2021-09-24 15:29:59 -0600

    home page view

--- src/main/java/com/project3/ecommerce/controllers/HomeController.java
+++ src/main/java/com/project3/ecommerce/controllers/HomeController.java
@@ -1,14 +1,15 @@
 package com.project3.ecommerce.controllers;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class HomeController {
 
-    @GetMapping("/index")
-    public String getIndex(){
+    @GetMapping("/home")
+    public String getIndex(Model model){
+        model.addAttribute("logoImage","logo1.png");
         return "Index";
     }
-
 }
