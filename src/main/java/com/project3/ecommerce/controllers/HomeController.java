0d7a1f4dd4aa326f0c45cf1971bccfde22f88d80
EGuerreroM <72275181+EGuerreroM@users.noreply.github.com>
2021-09-24 22:33:14 -0600

    HomePage finished
    finished functionality of home page some styling touches may have to be done

--- src/main/java/com/project3/ecommerce/controllers/HomeController.java
+++ src/main/java/com/project3/ecommerce/controllers/HomeController.java
@@ -3,13 +3,12 @@ package com.project3.ecommerce.controllers;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-
 @Controller
 public class HomeController {
-
     @GetMapping("/home")
     public String getIndex(Model model){
-        model.addAttribute("logoImage","logo1.png");
+        model.addAttribute("prodName","Gian penguin Shirt");
+        model.addAttribute("prodDescription","Super cool penguin shirt");
         return "Index";
     }
 
