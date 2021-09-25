75a5f201c18e41decaaf75d949a77727ab388497
victor <vpinedaa@unah.hn>
2021-09-25 00:13:49 -0600

    Create Pdf Report
    reporte pdf de productos

--- src/main/java/com/project3/ecommerce/controllers/ProductController.java
+++ src/main/java/com/project3/ecommerce/controllers/ProductController.java
@@ -1,12 +1,22 @@
 package com.project3.ecommerce.controllers;
 
-
+import com.itextpdf.html2pdf.ConverterProperties;
+import com.itextpdf.html2pdf.HtmlConverter;
 import com.project3.ecommerce.models.Category;
 import com.project3.ecommerce.models.Product;
 import com.project3.ecommerce.services.implementations.CategoryServiceImpl;
 import com.project3.ecommerce.services.implementations.ProductServiceImpl;
+import org.apache.tomcat.util.http.fileupload.ByteArrayOutputStream;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
+import org.thymeleaf.context.WebContext;
 
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
 import java.util.List;
 
 @RestController
@@ -58,5 +68,47 @@ public class ProductController {
 //
 //        return productService.saveProduct(foundedProduct)
 //    }
+    @RequestMapping(path = "/")
+    public String getOrderPage(Model model) throws IOException {
+        List<Product> listProductOrder = productServiceImpl.getAllProducts();
+        model.addAttribute("orderEntry", listProductOrder);
+        return "order";
+    }
+
+    @RequestMapping(path = "/pdf")
+    public ResponseEntity<?> getPDF(HttpServletRequest request, HttpServletResponse response) throws IOException {
+        /* Do Business Logic*/
+
+        //Order order = OrderHelper.getOrder();
+        List<Product> listProduct = productServiceImpl.getAllProducts();
+
+        /* Create HTML using Thymeleaf template Engine */
+
+        WebContext context = new WebContext(request, response, servletContext);
+        context.setVariable("productEntry", listProduct);
+        String orderHtml = templateEngine.process("product", context);
+
+        /* Setup Source and target I/O streams */
+
+        ByteArrayOutputStream target = new ByteArrayOutputStream();
+
+        /*Setup converter properties. */
+        ConverterProperties converterProperties = new ConverterProperties();
+        converterProperties.setBaseUri("http://localhost:8080");
+
+        /* Call convert method */
+        HtmlConverter.convertToPdf(orderHtml, target, converterProperties);
+
+        /* extract output as bytes */
+        byte[] bytes = target.toByteArray();
+
+
+        /* Send the response as downloadable PDF */
+
+        return ResponseEntity.ok()
+                .header(HttpHeaders.CONTENT_DISPOSITION, "attachment; filename=order.pdf")
+                .contentType(MediaType.APPLICATION_PDF)
+                .body(bytes);
+    }
 
 }
